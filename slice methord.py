8a05249c586427a2098ceb5fd55d6679160ca950
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-05-06 19:11:24 +0530

    Add files via upload

---
+++ slice methord.py
@@ -0,0 +1,8 @@
+#syntax: slice(start, stop, step)
+s="iit_Bombay"
+print(s[4:10])
+print(s[0:len(s):2])
+s1="iit-madras"
+print(s1[::]) #iit_madras
+print(s1[::-1]) #sardam_tii
+print(s[:-1]) #iit-madra
